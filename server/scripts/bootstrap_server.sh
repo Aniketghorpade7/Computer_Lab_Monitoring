d031727c42afd0120e1d30cb83f71e8673e0e8c3
Aniketghorpade7 <aniket.anil.ghorpade@gmail.com>
2026-02-12 17:35:55 +0530

    Almost completed the script

--- server/scripts/bootstrap_server.sh
+++ server/scripts/bootstrap_server.sh
@@ -7,6 +7,7 @@ set -x      #prints each command before excuting
 #Variable defining
 MONITOR_USER="monitor_admin"
 ALLOWED_PORTS=("80" "22" "3000" "9090")
+TIMEZONE="UTC"
 
 #Update the system first 
 apt-get update && apt-get upgrade -y
@@ -21,3 +22,43 @@ if ! getent passwd "$MONITOR_USER" > /dev/null; then
 else
     echo "User exist :)"
 fi
+
+#Configuring the firewall
+echo "Reseting the firewall to clean state"
+sudo ufw --force reset
+
+echo "Setting default policies"
+sudo ufw default deny incomming
+sudo ufw default allow outgoing
+
+echo "Allowing OpenSSH for remote logging"
+sudo ufw allow OpenSSH
+
+echo "Allowing the required ports"
+for PORT in "$ALLOWED_PORTS"
+do
+    sudo ufw allow "$PORT"
+done
+
+echo "Enabling the firewall"
+sudo ufw --force enable
+
+echo "Firewall status"
+sudo ufw status verbose
+
+#Setting up time zones
+
+echo "Setting up timezone to $TIMEZONE"
+sudo timedatectl set-timezone "$TIMEZONE"
+
+sudo systemctl enable systemd-timesyncd
+sudo systemctl start systemd-timesyncd
+sudo systemctl restart systemd-timesyncd
+
+sleep 3
+
+echo "Current time status:"
+timedatectl status
+
+echo "NTP status:"
+timedatectl show-timesync --all | head -n 15  
